c9bb1eaddeef28257a0be21c7325f381f6e2a1c7
Linux User <linuxuser@linux-d2006-13.domenis.local>
2025-10-16 22:23:04 +0300

    I forgot to commit inbetween exercises, but here are the solutions to exercise 3, 4 and 5.

--- nodes/localization/localizer.py
+++ nodes/localization/localizer.py
@@ -10,6 +10,8 @@ from pyproj import CRS, Transformer, Proj
 from novatel_oem7_msgs.msg import INSPVA
 from geometry_msgs.msg import PoseStamped, TwistStamped, Quaternion, TransformStamped
 
+import time
+
 class Localizer:
     def __init__(self):
 
@@ -27,11 +29,36 @@ class Localizer:
 
         # Subscribers
         rospy.Subscriber('/novatel/oem7/inspva', INSPVA, self.transform_coordinates)
+        rospy.Subscriber('/novatel/oem7/inspva', INSPVA, self.publish_pose_and_transform)
+        rospy.Subscriber('/novatel/oem7/inspva', INSPVA, self.publish_velocity)
 
         # Publishers
         self.current_pose_pub = rospy.Publisher('current_pose', PoseStamped, queue_size=10)
         self.current_velocity_pub = rospy.Publisher('current_velocity', TwistStamped, queue_size=10)
         self.br = TransformBroadcaster()
+
+        # to publish current pose, velocity and transform
+        self.current_pose_msg = PoseStamped()
+        self.current_velocity_msg = TwistStamped()
+        self.current_transform_msg = TransformStamped()
+
+
+    # convert azimuth to yaw angle
+    def convert_azimuth_to_yaw(self, azimuth):
+        """
+        Converts azimuth to yaw. Azimuth is CW angle from the north. Yaw is CCW angle from the East.
+        :param azimuth: azimuth in radians
+        :return: yaw in radians
+        """
+        yaw = -azimuth + math.pi / 2
+        # Clamp within 0 to 2 pi
+        if yaw > 2 * math.pi:
+            yaw = yaw - 2 * math.pi
+        elif yaw < 0:
+            yaw += 2 * math.pi
+
+        return yaw
+
 
     def transform_coordinates(self, msg):
         # use the transformer to transform the coordinates from the INSPVA message
@@ -40,8 +67,50 @@ class Localizer:
         transformed_y -= self.origin_y
         print(f'Latitude: {transformed_x}, longitude: {transformed_y}')
 
+
+    def publish_pose_and_transform(self, msg):
+        transformed_x, transformed_y = self.transformer.transform(msg.latitude, msg.longitude)
+        transformed_x -= self.origin_x
+        transformed_y -= self.origin_y
+        print(f'Latitude: {transformed_x}, longitude: {transformed_y}')
+
+        azimuth_correction = self.utm_projection.get_factors(msg.longitude, msg.latitude).meridian_convergence
+        yaw = self.convert_azimuth_to_yaw(azimuth_correction) # Convert yaw to quaternion
+
+        x, y, z, w = quaternion_from_euler(0, 0, yaw)
+        orientation = Quaternion(x, y, z, w)
+
+        #self.current_pose_msg.header.stamp = msg.time
+        # i don't know how to take the stamp from INSPVA message header
+        self.current_pose_msg.header.frame_id = "map"
+        self.current_pose_msg.pose.position.x = transformed_x
+        self.current_pose_msg.pose.position.y = transformed_y
+        self.current_pose_msg.pose.position.z = msg.height - self.undulation
+        self.current_pose_msg.pose.orientation = orientation
+        self.current_pose_pub.publish(self.current_pose_msg)
+
+        self.current_transform_msg.header.frame_id = "map"
+        self.current_transform_msg.child_frame_id = "base_link"
+        self.current_transform_msg.transform.translation.x = 0 + self.current_pose_msg.pose.position.x
+        self.current_transform_msg.transform.translation.y = 0 + self.current_pose_msg.pose.position.y
+        self.current_transform_msg.transform.translation.z = 0 + self.current_pose_msg.pose.position.z
+        self.current_transform_msg.transform.rotation = self.current_pose_msg.pose.orientation
+        self.br.sendTransform(self.current_transform_msg)
+        #self.current_velocity_msg.header.stamp = msg.secs
+        # i don't know how to take the stamp from INSPVA message header
+
+
+    def publish_velocity(self, msg):
+        #self.current_velocity_msg.header.stamp = msg.secs
+        # i don't know how to take the stamp from INSPVA message header
+        self.current_velocity_msg.header.frame_id = "base_link"
+        self.current_velocity_msg.twist.linear.x = (msg.north_velocity**2 + msg.east_velocity**2)**0.5
+        self.current_velocity_pub.publish(self.current_velocity_msg)
+
+
     def run(self):
         rospy.spin()
+
 
 if __name__ == '__main__':
     rospy.init_node('localizer')
