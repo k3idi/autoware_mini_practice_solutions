89dcc6001e8ea13b1ab3d6f352b099b744a8e6e0
k3idi <155574583+k3idi@users.noreply.github.com>
2025-10-24 02:17:49 +0300

    Task 5 and 6

--- nodes/control/pure_pursuit_follower.py
+++ nodes/control/pure_pursuit_follower.py
@@ -12,11 +12,14 @@ from shapely import prepare, distance
 
 from tf.transformations import euler_from_quaternion
 
+from scipy.interpolate import interp1d
+
 class PurePursuitFollower:
     def __init__(self):
 
         # Parameters
         self.path_linestring = None
+        self.distance_to_velocity_interpolator = None
 
         # Publishers
         self.vehicle_cmd_pub = rospy.Publisher('/control/vehicle_cmd', VehicleCmd, queue_size=10 )
@@ -37,6 +40,18 @@ class PurePursuitFollower:
         prepare(path_linestring)
         self.path_linestring = path_linestring
 
+        # Create a distance-to-velocity interpolator for the path
+        # collect waypoint x and y coordinates
+        waypoints_xy = np.array([(w.position.x, w.position.y) for w in msg.waypoints])
+        # Calculate distances between points
+        distances = np.cumsum(np.sqrt(np.sum(np.diff(waypoints_xy, axis=0)**2, axis=1)))
+        # add 0 distance in the beginning
+        distances = np.insert(distances, 0, 0)
+        # Extract velocity values at waypoints
+        velocities = np.array([w.speed for w in msg.waypoints])
+
+        self.distance_to_velocity_interpolator = interp1d(distances, velocities, kind='linear', bounds_error=False, fill_value=0.0)
+
     def current_pose_callback(self, msg):
         #print(f'x: {msg.pose.position.x}, {msg.pose.position.y}')
         self.vehicle_cmd_msg.header.stamp = msg.header.stamp
@@ -48,19 +63,21 @@ class PurePursuitFollower:
             d_ego_from_path_start = self.path_linestring.project(current_pose)
             print(f'd_ego_from_path_start = {d_ego_from_path_start}')
 
-        # Reading in the parameter values
         lookahead_distance = rospy.get_param("~lookahead_distance")
         wheel_base = rospy.get_param("/vehicle/wheel_base")
 
-        # using euler_from_quaternion to get the heading angle
-        _, _, heading = euler_from_quaternion([msg.pose.orientation.x, msg.pose.orientation.y,
+        _, _, heading_angle = euler_from_quaternion([msg.pose.orientation.x, msg.pose.orientation.y,
                                                msg.pose.orientation.z, msg.pose.orientation.w])
 
-        # Missing: lookahead point calculation.
-        lookahead_point = 0
+        lookahead_point = self.path_linestring.interpolate(lookahead_distance)
+        lookahead_heading_angle = np.arctan2(lookahead_point.y - current_pose.y, lookahead_point.x - current_pose.x)
+        
+        ld = ((lookahead_point.y - current_pose.y)**2 + (lookahead_point.x - current_pose.x)**2)**0.5
+        alpha = lookahead_heading_angle - heading_angle
 
-        # lookahead point heading calculation
-        lookahead_heading = np.arctan2(lookahead_point.y - current_pose.y, lookahead_point.x - current_pose.x)
+        self.vehicle_cmd_msg.ctrl_cmd.steering_angle = np.arctan2(2 * wheel_base * np.sin(alpha) / ld)
+        self.vehicle_cmd_msg.ctrl_cmd.linear_velocity = self.distance_to_velocity_interpolator(d_ego_from_path_start)
+
 
 
 
