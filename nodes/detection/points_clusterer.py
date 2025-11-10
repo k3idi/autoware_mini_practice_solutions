8911097fc4d854e21d396133d06d362fb3e31362
k3idi <155574583+k3idi@users.noreply.github.com>
2025-11-10 23:04:16 +0200

    Task 4: filtering and concatenating points and labels

--- nodes/detection/points_clusterer.py
+++ nodes/detection/points_clusterer.py
@@ -17,24 +17,37 @@ class PointsClusterer:
         self.clusterer = DBSCAN(eps=self.cluster_epsilon, min_samples=self.cluster_min_size)
         
         # Publishers
-
+        self.points_clustered_pub = rospy.Publisher('points_clustered', PointCloud2, queue_size=1, tcp_nodelay=True)
 
         # Subscribers
         rospy.Subscriber('points_filtered', PointCloud2, self.points_callback, queue_size=1, buff_size=2**24, tcp_nodelay=True)
 
-        # Messages
-
-
     def points_callback(self, msg):
         data = numpify(msg)
         points = structured_to_unstructured(data[['x', 'y', 'z']], dtype=np.float32)
-        print(f'points shape: {points.shape}')
 
         labels = self.clusterer.fit_predict(points)
-        print(f'labels shape: {labels.shape}')
 
         assert points.shape[0] == labels.shape[0], "there is a different amound of points and labels"
-  
+
+        points_labelled = []
+        for i in range(len(points)):
+            if labels[i] != -1:
+                points_labelled.append((points[i][0], points[i][1], points[i][2], labels[i]))
+
+        data = unstructured_to_structured(points_labeled, dtype=np.dtype([
+           ('x', np.float32),
+           ('y', np.float32),
+           ('z', np.float32),
+           ('label', np.int32)
+        ]))
+
+        cluster_msg = msgify(PointCloud2, data)
+        cluster_msg.header.stamp = msg.header.stamp
+        cluster_msg.header.frame_id = msg.header.frame_id
+
+        self.points_clustered_pub.publish(cluster_msg)
+    
     def run(self):
         rospy.spin()
 
