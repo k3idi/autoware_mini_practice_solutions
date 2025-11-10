27cd4007049e642294f46675ea885d23c21adfa9
k3idi <155574583+k3idi@users.noreply.github.com>
2025-11-10 22:15:20 +0200

    Task 3, creating the clusterer

--- nodes/detection/points_clusterer.py
+++ nodes/detection/points_clusterer.py
@@ -4,6 +4,7 @@ import rospy
 import numpy as np
 from numpy.lib.recfunctions import structured_to_unstructured, unstructured_to_structured
 from ros_numpy import numpify, msgify
+from sklearn.cluster import DBSCAN
 
 from sensor_msgs.msg import PointCloud2
 
@@ -11,7 +12,9 @@ class PointsClusterer:
     def __init__(self):
 
         # Parameters (not sure if they are necessary)
-
+        self.cluster_epsilon = rospy.get_param("~cluster_epsilon")
+        self.cluster_min_size = rospy.get_param("~cluster_min_size")
+        self.clusterer = DBSCAN(eps=self.cluster_epsilon, min_samples=self.cluster_min_size)
         
         # Publishers
 
@@ -25,7 +28,12 @@ class PointsClusterer:
     def points_callback(self, msg):
         data = numpify(msg)
         points = structured_to_unstructured(data[['x', 'y', 'z']], dtype=np.float32)
-        print(f'points shape: {points}')
+        print(f'points shape: {points.shape}')
+
+        labels = self.clusterer.fit_predict(points)
+        print(f'labels shape: {labels.shape}')
+
+        assert points.shape[0] == labels.shape[0], "there is a different amound of points and labels"
   
     def run(self):
         rospy.spin()
