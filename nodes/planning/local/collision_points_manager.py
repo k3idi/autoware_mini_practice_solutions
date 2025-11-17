772e16af5705fdc4a8f28ff8cb2241075e849941
k3idi <155574583+k3idi@users.noreply.github.com>
2025-11-17 23:22:04 +0200

    Began with task 3.

---
+++ nodes/planning/local/collision_points_manager.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+import rospy
+import shapely
+import math
+import numpy as np
+import threading
+from ros_numpy import msgify
+from autoware_mini.msg import Path, DetectedObjectArray
+from sensor_msgs.msg import PointCloud2
+from shapely.geometry import LineString
+from shapely import prepare, buffer
+
+DTYPE = np.dtype([
+    ('x', np.float32),
+    ('y', np.float32),
+    ('z', np.float32),
+    ('vx', np.float32),
+    ('vy', np.float32),
+    ('vz', np.float32),
+    ('distance_to_stop', np.float32),
+    ('deceleration_limit', np.float32),
+    ('category', np.int32)
+])
+
+class CollisionPointsManager:
+
+    def __init__(self):
+
+        # parameters
+        self.safety_box_width = rospy.get_param("safety_box_width")
+        self.stopped_speed_limit = rospy.get_param("stopped_speed_limit")
+        self.braking_safety_distance_obstacle = rospy.get_param("~braking_safety_distance_obstacle")
+
+        # variables
+        self.detected_objects = None
+
+        # Lock for thread safety
+        self.lock = threading.Lock()
+
+        # publishers
+        self.local_path_collision_pub = rospy.Publisher('collision_points', PointCloud2, queue_size=1, tcp_nodelay=True)
+
+        # subscribers
+        rospy.Subscriber('extracted_local_path', Path, self.path_callback, queue_size=1, tcp_nodelay=True)
+        rospy.Subscriber('/detection/final_objects', DetectedObjectArray, self.detected_objects_callback, queue_size=1, buff_size=2**20, tcp_nodelay=True)
+
+    def detected_objects_callback(self, msg):
+        self.detected_objects = msg.objects
+
+    def path_callback(self, msg):
+        with self.lock:
+            detected_objects = self.detected_objects
+        collision_points = np.array([], dtype=DTYPE)
+      
+        if len(msg.waypoints) == 0: # maybe this check won't work, i have to check in local_path_extractor.py
+            local_path_collision_msg = msgify(PointCloud2, collision_points)
+            local_path_collision_msg.header.stamp = msg.header.stamp
+            local_path_collision_msg.header.frame_id = msg.header.frame_id
+            self.local_path_collision_pub.publish(local_path_collision_msg)
+            return # maybe change this to something else
+          
+        path_linestring = LineString([(w.position.x, w.position.y) for w in msg.waypoints])
+        # prepare(path_linestring) # prepare path - creates spatial tree, making the spatial queries more efficient
+        
+    
+
+    def run(self):
+        rospy.spin()
+
+if __name__ == '__main__':
+    rospy.init_node('collision_points_manager')
+    node = CollisionPointsManager()
+    node.run()
